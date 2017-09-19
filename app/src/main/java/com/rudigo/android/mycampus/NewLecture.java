b57217ba88051baef5802eb8b6d79ac261abbc92
ikhiloya <imokhaiikhiloya@gmail.com>
2017-09-19 14:01:20 +0100

    Initial commit

---
+++ app/src/main/java/com/rudigo/android/mycampus/NewLecture.java
@@ -0,0 +1,13 @@
+package com.rudigo.android.mycampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewLecture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_lecture);
+    }
+}
