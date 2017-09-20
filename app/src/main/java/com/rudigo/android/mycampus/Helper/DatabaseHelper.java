82350ef1e8cae22f73c0004da9b78eab34c66497
itswisdomagain <wisdom.arerosuoghene@gmail.com>
2017-09-20 20:08:27 +0100

    implemented time picker

---
+++ app/src/main/java/com/rudigo/android/mycampus/Helper/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.rudigo.android.mycampus.Helper;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.android.AndroidContext;
+
+import java.io.IOException;
+
+
+public class DatabaseHelper {
+
+    // database helper used to store data offline
+
+    public static final String LECTURE_DATA = "lecture";
+
+    public static Database getDatabase(Context context, String databaseName){
+        try {
+            AndroidContext androidContext = new AndroidContext(context);
+            Manager dbManager = new Manager(androidContext, Manager.DEFAULT_OPTIONS);
+            return dbManager.getDatabase(databaseName);
+        } catch (CouchbaseLiteException | IOException e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Cannot get access to required database!", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+}}
+
